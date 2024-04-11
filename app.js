1d871eb32edcac75d5693687197f51a8ab247acc
Thinh <155166003+NHThinh123@users.noreply.github.com>
2024-04-11 23:03:07 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const booksRouter = require("./app/routes/book.route");
 
-// const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -16,22 +16,22 @@ app.get("/", (req, res) => {
 
 app.use("/api/books", booksRouter);
 
-// // handle 404 response
-// app.use((req, res, next) => {
-//   //Code ở đay sẽ chạy khi không có route được định nghĩa nào
-//   //      khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
-//   return next(new ApiError(404, "Resource not found"));
-// });
+// handle 404 response
+app.use((req, res, next) => {
+  //Code ở đay sẽ chạy khi không có route được định nghĩa nào
+  //      khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+  return next(new ApiError(404, "Resource not found"));
+});
 
-// // define error-handling middware last, after other app.use() and routes calls
-// app.use((err, req, res, next) => {
-//   // Middware xử lý lỗi tập trung.
-//   //Trong các đoạn coade xử lý các route, gọi next(error)
-//   //      sẽ chuyển về middware xử lý lỗi này
+// define error-handling middware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  // Middware xử lý lỗi tập trung.
+  //Trong các đoạn coade xử lý các route, gọi next(error)
+  //      sẽ chuyển về middware xử lý lỗi này
 
-//   return res.status(err.statusCode || 500).json({
-//     message: err.message || "Internal Server Error",
-//   });
-// });
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;

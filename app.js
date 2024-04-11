3545a3924e100d010a0a7bef057bad9adccc42dc
Thinh <155166003+NHThinh123@users.noreply.github.com>
2024-04-11 22:40:37 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+
+// const ApiError = require("./app/api-error");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Chào mừng đến với website mượn sách online" });
+});
+
+// // app.use("/api/contacts", contactsRouter);
+
+// // handle 404 response
+// app.use((req, res, next) => {
+//   //Code ở đay sẽ chạy khi không có route được định nghĩa nào
+//   //      khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+//   return next(new ApiError(404, "Resource not found"));
+// });
+
+// // define error-handling middware last, after other app.use() and routes calls
+// app.use((err, req, res, next) => {
+//   // Middware xử lý lỗi tập trung.
+//   //Trong các đoạn coade xử lý các route, gọi next(error)
+//   //      sẽ chuyển về middware xử lý lỗi này
+
+//   return res.status(err.statusCode || 500).json({
+//     message: err.message || "Internal Server Error",
+//   });
+// });
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require("./app");
+const config = require("./app/config");
+// const MongoDB = require("./app/utils/mongodb.util");
+
+async function startServer() {
+  try {
+    // await MongoDB.connect(config.db.uri);
+    // console.log("kết nối thành công với cơ sở dữ liệu");
+
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server đang chạy ở port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Không thể kết nối với cơ sở dữ liệu", error);
+    process.exit();
+  }
+}
+
+startServer();
